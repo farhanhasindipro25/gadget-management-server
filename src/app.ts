ab923373a7e8c58d53dc45982c39869bf059c8f4
farhanhasindipro25 <farhan.hasin.25@gmail.com>
2024-01-27 02:47:06 +0600

    Chore: Setup prettier and eslint

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,15 @@
-import { Application, Request, Response } from "express";
-import express from "express";
-import cors from "cors";
+import { Application, Request, Response } from 'express'
+import express from 'express'
+import cors from 'cors'
 
-const app: Application = express();
+const app: Application = express()
 
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Server running successfully!");
-});
+app.get('/', (req: Request, res: Response) => {
+  res.send('Server running successfully!')
+})
 
-export default app;
+export default app
