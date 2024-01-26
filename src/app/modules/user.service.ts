d5542d586d010c29f6bfed75c8ebb12e354cd323
farhanhasindipro25 <farhan.hasin.25@gmail.com>
2024-01-27 03:14:46 +0600

    Chore: Added user interface and model

---
+++ src/app/modules/user.service.ts
@@ -0,0 +1,13 @@
+import { IUser } from './user.interface'
+import { User } from './user.model'
+
+const createUser = async (user: IUser): Promise<IUser | null> => {
+  const createdUser = await User.create(user)
+
+  if (!createUser) {
+    throw new Error('Failed to create user')
+  }
+  return createdUser
+}
+
+export default { createUser }
