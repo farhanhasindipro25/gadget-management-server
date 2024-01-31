8e63de5a26c6a44dba819113ae75c39969d2f93d
farhanhasindipro25 <farhan.hasin.25@gmail.com>
2024-02-01 02:45:59 +0600

    chore:Maintained well formatted logs

--- src/common/logger.ts
+++ src/common/logger.ts
@@ -1,26 +1,48 @@
-import winston from 'winston'
+/* eslint-disable no-undef */
+import path from 'path'
+import { createLogger, format, transports } from 'winston'
+const { combine, timestamp, label, printf } = format
 
-const infoLogger = winston.createLogger({
+//custom log format
+const myFormat = printf(({ level, message, label, timestamp }) => {
+  const date = new Date(timestamp)
+  const hours = date.getHours()
+  const minutes = date.getMinutes()
+  const seconds = date.getSeconds()
+  return `${date.toDateString()} ${hours}:${minutes}:${seconds} [${label}] ${level}: ${message}`
+})
+
+const infoLogger = createLogger({
   level: 'info',
-  format: winston.format.json(),
+  format: combine(
+    label({ label: 'Gadget Management' }),
+    timestamp(),
+    myFormat,
+    // prettyPrint(),
+  ),
   transports: [
-    new winston.transports.Console(),
-    // new winston.transports.File({
-    //   filename: path.join(process.cwd(), 'logs', 'winston', 'success.log'),
-    //   level: 'info',
-    // }),
+    new transports.Console(),
+    new transports.File({
+      filename: path.join(process.cwd(), 'logs', 'winston', 'success.log'),
+      level: 'info',
+    }),
   ],
 })
 
-const errorLogger = winston.createLogger({
+const errorLogger = createLogger({
   level: 'error',
-  format: winston.format.json(),
+  format: combine(
+    label({ label: 'Gadget Management' }),
+    timestamp(),
+    myFormat,
+    // prettyPrint(),
+  ),
   transports: [
-    new winston.transports.Console(),
-    // new winston.transports.File({
-    //   filename: path.join(process.cwd(), 'logs', 'winston', 'error.log'),
-    //   level: 'error',
-    // }), 
+    new transports.Console(),
+    new transports.File({
+      filename: path.join(process.cwd(), 'logs', 'winston', 'error.log'),
+      level: 'error',
+    }),
   ],
 })
 
