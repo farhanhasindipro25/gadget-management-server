9f4d210629baa8740f192c0c5029b9a79cfe662b
farhanhasindipro25 <farhan.hasin.25@gmail.com>
2024-02-14 03:08:29 +0600

    Refactor: Set refresh token in cookies

--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -2,18 +2,25 @@ import { Request, Response } from 'express';
 import httpStatus from 'http-status';
 import catchAsync from '../../../common/helpers/catchAsync';
 import sendResponse from '../../../common/helpers/sendResponse';
+import config from '../../../config';
 import { ILoginUserResponse } from './auth.interface';
 import { AuthService } from './auth.service';
 
 const loginUser = catchAsync(async (req: Request, res: Response) => {
   const { ...loginData } = req.body;
   const result = await AuthService.loginUser(loginData);
+  const { refreshToken, ...data } = result;
 
+  const cookieOptions = {
+    secure: config.env === 'production',
+    httpOnly: true,
+  };
+  res.cookie('refreshToken', refreshToken, cookieOptions);
   sendResponse<ILoginUserResponse>(res, {
     statusCode: httpStatus.OK,
     success: true,
     message: 'User login successful!',
-    data: result,
+    data: data,
   });
 });
 
